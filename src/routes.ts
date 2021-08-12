fe7168821b007853910d5b8a0d4def654bdf8cb6
lmorais <leozonnn@gmail.com>
2021-08-12 12:01:10 -0300

    feat: implementation first route to send email and package json new depedencies

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import SendMessageService from './services/SendMessageService';
+
 const routes = Router();
+
+routes.post('/messages', async (request, response) => {
+  const sendMessage = new SendMessageService();
+
+  await sendMessage.run();
+
+  return response.json({ Ok: true });
+});
 
 export default routes;
