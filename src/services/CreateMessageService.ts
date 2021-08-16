b3fddb2ce98fadcadbd5227e88983cf3624b0b36
lmorais <leozonnn@gmail.com>
2021-08-15 22:38:56 -0300

    feat: implementation service to add new message of emails and tags

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,34 @@
+import MailQueue from '../queues/MailQueue';
+import Contact from '../schemas/Contact';
+import Message, { MessageModel } from '../schemas/Message';
+
+class SendMessageService {
+  async run(
+    messageData: {
+      subject: string;
+      body: string;
+    },
+    tags: string[]
+  ): Promise<MessageModel> {
+    const message = await Message.create(messageData);
+
+    const recipients = await Contact.find({
+      tags: {
+        $in: tags,
+      },
+    });
+
+    await Promise.all(
+      recipients.map((recipient) => {
+        return MailQueue.add({
+          to: recipient.email,
+          messageData,
+        });
+      })
+    );
+
+    return message;
+  }
+}
+
+export default SendMessageService;
