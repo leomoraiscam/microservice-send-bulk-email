70d37e678d7489ee70d801884e9fa1872d47c585
lmorais <leozonnn@gmail.com>
2021-08-15 22:37:53 -0300

    feat: implementation a service to send email

---
+++ src/services/SendMessageToRecipientService.ts
@@ -0,0 +1,28 @@
+import nodemailer, { Transporter } from 'nodemailer';
+
+class SendMessageToRecipientService {
+  private transporter: Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      secure: false,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
+  }
+
+  async run(): Promise<void> {
+    await this.transporter.sendMail({
+      from: 'sender@example.com',
+      to: 'equipe@gobarber.com.br',
+      subject: 'Message',
+      text: 'I hope this message gets delivered!',
+    });
+  }
+}
+
+export default SendMessageToRecipientService;
