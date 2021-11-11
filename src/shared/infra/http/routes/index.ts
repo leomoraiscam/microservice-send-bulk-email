c69ed4e19ddc136b1dfbcf5ad40ca5951ad78109
lmorais <leozonnn@gmail.com>
2021-11-11 16:03:41 -0300

    feat: implementation a default shared routes and resources

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,36 +1,11 @@
 import { Router } from 'express';
-import fs from 'fs';
-import path from 'path';
 
-import ImportContactsService from '@modules/contacts/services/ImportContactsService';
-import CreateMessageService from '@modules/messages/services/CreateMessageService';
+import contactRoutes from '@modules/contacts/infra/http/routes/contact';
+import messageRoutes from '@modules/messages/infra/http/routes/message';
 
 const routes = Router();
 
-routes.post('/contacts/import', async (request, response) => {
-  const { tags } = request.body;
-
-  const contactsReadStream = fs.createReadStream(
-    path.resolve(__dirname, '..', 'tmp', 'contacts_mail.csv')
-  );
-
-  const importContacts = new ImportContactsService();
-
-  await importContacts.run(contactsReadStream, tags);
-
-  return response.send();
-});
-
-routes.post('/messages', async (request, response) => {
-  const { subject, body, tags } = request.body;
-
-  const createMessage = new CreateMessageService();
-
-  const messageData = { subject, body };
-
-  const message = await createMessage.run(messageData, tags);
-
-  return response.json(message);
-});
+routes.use('/contacts', contactRoutes);
+routes.use('/messages', messageRoutes);
 
 export default routes;
