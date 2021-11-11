71699872737fb8110101c538ec2eac3d08b03b06
lmorais <leozonnn@gmail.com>
2021-11-11 15:59:58 -0300

    chore: definition default port when a non-exist a the same in dotenv

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -8,7 +8,7 @@ interface IMongoConfig {
 
 export default {
   host: process.env.MONGO_URL || 'localhost',
-  port: Number(process.env.MONGO_PORT),
+  port: Number(process.env.MONGO_PORT) || 27017,
   username: process.env.MONGO_USER,
   password: process.env.MONGO_PASS,
   database: process.env.MONGO_DB,
