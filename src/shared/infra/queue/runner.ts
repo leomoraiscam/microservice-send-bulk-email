8e891ea25fcc31b758efc5419a2949372e6bbfc9
lmorais <leozonnn@gmail.com>
2021-11-11 16:01:47 -0300

    chore: change incorrect name of file to correct name

---
+++ src/shared/infra/queue/runner.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import MailQueue from '@config/redis';
+import SendMessageToRecipientService from '@modules/messages/services/SendMessageToRecipientService';
+
+MailQueue.process(async () => {
+  const sendMessageToRecipient = new SendMessageToRecipientService();
+
+  await sendMessageToRecipient.run();
+});
