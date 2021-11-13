4dd82ec3655bc02f57915e19c96f13005d3ab490
lmorais <leozonnn@gmail.com>
2021-11-12 23:08:40 -0300

    feat: implementataion error handling in applications

--- src/shared/infra/app.ts
+++ src/shared/infra/app.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import 'express-async-errors';
+import AppError from '@shared/errors/AppError';
 
 import routes from './http/routes/index';
 import '@shared/infra/mongoose/connection';
@@ -9,6 +12,24 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+        status: 'Error',
+      });
+    }
+
+    console.log('Error: ', error.message);
+
+    return response.status(500).json({
+      message: 'Internal Server Error',
+      status: 'Error',
+    });
+  }
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server is running');
