88ff574166d12cd0c27ad53071035ce40bcff575
lmorais <leozonnn@gmail.com>
2021-11-12 22:44:19 -0300

    chore: separate contacts routes of process requests

--- src/modules/contacts/infra/http/routes/contact.ts
+++ src/modules/contacts/infra/http/routes/contact.ts
@@ -1,33 +1,11 @@
 import { Router } from 'express';
-import fs from 'fs';
-import path from 'path';
 
-import ImportContactsService from '@modules/contacts/services/ImportContactsService';
+import ImportContactsController from '@modules/contacts/infra/http/controllers/ImportContactsController';
+
+const importContactsController = new ImportContactsController();
 
 const contactsRoutes = Router();
 
-contactsRoutes.post('/import', async (request, response) => {
-  const { tags } = request.body;
-
-  const contactsReadStream = fs.createReadStream(
-    path.resolve(
-      __dirname,
-      '..',
-      '..',
-      '..',
-      '..',
-      '..',
-      '..',
-      'tmp',
-      'contacts_mail.csv'
-    )
-  );
-
-  const importContacts = new ImportContactsService();
-
-  await importContacts.run(contactsReadStream, tags);
-
-  return response.send();
-});
+contactsRoutes.post('/import', importContactsController.handle);
 
 export default contactsRoutes;
