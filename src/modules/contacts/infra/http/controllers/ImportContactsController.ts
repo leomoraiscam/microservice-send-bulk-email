e39ca3a69c090a97df8bcbac515ecf7b6b193b55
lmorais <leozonnn@gmail.com>
2021-11-12 22:43:35 -0300

    feat: implementation controller to import contacts

---
+++ src/modules/contacts/infra/http/controllers/ImportContactsController.ts
@@ -0,0 +1,33 @@
+import { Response, Request } from 'express';
+import fs from 'fs';
+import path from 'path';
+
+import ImportContactsService from '@modules/contacts/services/ImportContactsService';
+
+class ImportContactsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { tags } = request.body;
+
+    const contactsReadStream = fs.createReadStream(
+      path.resolve(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        '..',
+        '..',
+        '..',
+        'tmp',
+        'contacts_mail.csv'
+      )
+    );
+
+    const importContacts = new ImportContactsService();
+
+    await importContacts.execute(contactsReadStream, tags);
+
+    return response.send();
+  }
+}
+
+export default ImportContactsController;
