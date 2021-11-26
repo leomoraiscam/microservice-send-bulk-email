7f4e245f68e9eb9f0a99bd0a6a38b5f9fd73188d
lmorais <leozonnn@gmail.com>
2021-11-26 14:02:47 -0300

    feat: configuration multer in contact route

--- src/modules/contacts/infra/http/routes/contact.ts
+++ src/modules/contacts/infra/http/routes/contact.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import upload from '@config/upload';
 import CreateTagContactServiceController from '@modules/contacts/infra/http/controllers/CreateTagContactServiceController';
 import ImportContactsController from '@modules/contacts/infra/http/controllers/ImportContactsController';
 
@@ -8,8 +10,13 @@ const createTagContactServiceController =
   new CreateTagContactServiceController();
 
 const contactsRoutes = Router();
+const uploadContacts = multer(upload.multer);
 
-contactsRoutes.post('/import', importContactsController.handle);
+contactsRoutes.post(
+  '/import',
+  uploadContacts.single('file'),
+  importContactsController.handle
+);
 contactsRoutes.post('/tags/:id', createTagContactServiceController.handle);
 
 export default contactsRoutes;
