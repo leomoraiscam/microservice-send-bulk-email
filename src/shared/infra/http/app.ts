9c63dae9267bc6e7106438f89f184b01b4ea823f
lmorais <leozonnn@gmail.com>
2021-12-05 19:34:29 -0300

    feat: implementation bull dashboard to monitoring queue process and data

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,7 +1,11 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import Queue from 'bull';
 import express, { Request, Response, NextFunction } from 'express';
 
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { ExpressAdapter } from '@bull-board/express';
 import 'express-async-errors';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
@@ -10,7 +14,19 @@ import routes from './routes';
 
 import '@shared/container';
 
+const someQueue = new Queue('mail');
+
+const serverAdapter = new ExpressAdapter();
+
+createBullBoard({
+  queues: [new BullAdapter(someQueue)],
+  serverAdapter,
+});
+
 const app = express();
+
+serverAdapter.setBasePath('/admin/queues');
+app.use('/admin/queues', serverAdapter.getRouter());
 
 app.use(express.json());
 
