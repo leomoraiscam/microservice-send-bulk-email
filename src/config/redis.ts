a9dd0646584fe2b5fe1ad711f118ab3a593894b8
lmorais <leozonnn@gmail.com>
2022-01-18 20:51:07 -0300

    chore: remove password in redis config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,5 +5,4 @@ type RedisConfig = RedisOptions;
 export default {
   host: process.env.REDIS_URL,
   port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASS,
 } as RedisConfig;
