53f5d8c03ea80dec36994e172b7e678ba64f8f55
lmorais <leozonnn@gmail.com>
2022-01-19 22:57:29 -0300

    chore: definition users and session routes in application index

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,15 @@ import { Router } from 'express';
 import contactRoutes from '@modules/contacts/infra/http/routes/contact';
 import tagsRoutes from '@modules/contacts/infra/http/routes/tags';
 import messageRoutes from '@modules/messages/infra/http/routes/message';
+import sessionRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import userRoutes from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/contacts', contactRoutes);
 routes.use('/tags', tagsRoutes);
 routes.use('/messages', messageRoutes);
+routes.use('/users', userRoutes);
+routes.use('/sessions', sessionRoutes);
 
 export default routes;
