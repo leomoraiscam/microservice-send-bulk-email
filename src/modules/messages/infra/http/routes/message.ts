269c2fde7f22283a6e87e94d1bdf9c71f722e6f9
lmorais <leozonnn@gmail.com>
2022-01-25 01:17:58 -0300

    feat: use roles and permissions midlewares

--- src/modules/messages/infra/http/routes/message.ts
+++ src/modules/messages/infra/http/routes/message.ts
@@ -2,13 +2,20 @@ import { Router } from 'express';
 
 import CreateMessageController from '@modules/messages/infra/http/controllers/CreateMessageController';
 import SendMessageController from '@modules/messages/infra/http/controllers/SendMessageController';
+import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
+import can from '@shared/infra/http/middlewares/ensurePermission';
 
 const messagesRoutes = Router();
 
 const createMessageController = new CreateMessageController();
 const sendMessageController = new SendMessageController();
 
-messagesRoutes.post('/', createMessageController.handle);
+messagesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  can(['send_mail']),
+  createMessageController.handle
+);
 messagesRoutes.post('/:id/send', sendMessageController.handle);
 
 export default messagesRoutes;
--- src/modules/roles/infra/http/routes/roles.ts
+++ src/modules/roles/infra/http/routes/roles.ts
@@ -2,11 +2,17 @@ import { Router } from 'express';
 
 import RolesController from '@modules/roles/infra/http/controller/RolesController';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { is } from '@shared/infra/http/middlewares/ensurePermission';
 
 const rolesController = new RolesController();
 
 const rolesRoutes = Router();
 
-rolesRoutes.post('/', ensureAuthenticated, rolesController.handle);
+rolesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  is(['admin']),
+  rolesController.handle
+);
 
 export default rolesRoutes;
