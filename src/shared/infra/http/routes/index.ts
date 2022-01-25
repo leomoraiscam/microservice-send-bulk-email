b0c6652a3f805003e7dfa5e5800630f7ca71fd4c
lmorais <leozonnn@gmail.com>
2022-01-24 23:00:09 -0300

    feat: definition acl routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import contactRoutes from '@modules/contacts/infra/http/routes/contact';
 import tagsRoutes from '@modules/contacts/infra/http/routes/tags';
 import messageRoutes from '@modules/messages/infra/http/routes/message';
+import permissionsRoutes from '@modules/roles/infra/http/routes/permissions';
+import rolesRoutes from '@modules/roles/infra/http/routes/roles';
 import sessionRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import userRoutes from '@modules/users/infra/http/routes/users.routes';
 
@@ -13,5 +15,7 @@ routes.use('/tags', tagsRoutes);
 routes.use('/messages', messageRoutes);
 routes.use('/users', userRoutes);
 routes.use('/sessions', sessionRoutes);
+routes.use('/roles', rolesRoutes);
+routes.use('/permissions', permissionsRoutes);
 
 export default routes;
