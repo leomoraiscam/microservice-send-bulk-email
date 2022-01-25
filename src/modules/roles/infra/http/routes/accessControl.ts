d68992d1ef29e0dfc27528cf8b2434a4d3489a7f
lmorais <leozonnn@gmail.com>
2022-01-25 01:17:08 -0300

    feat: definition route to atribute role an users

--- src/modules/roles/infra/http/routes/accessControl.ts
+++ src/modules/roles/infra/http/routes/accessControl.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import CreateRolePermissionController from '@modules/roles/infra/http/controller/CreateRolePermissionController';
 import CreateUserAccessControlListController from '@modules/roles/infra/http/controller/CreateUserAccessControlListController';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const createUserAccessControlListController =
   new CreateUserAccessControlListController();
+const createRolePermissionController = new CreateRolePermissionController();
 
 const userAclRoutes = Router();
 
@@ -14,4 +16,8 @@ userAclRoutes.post(
   createUserAccessControlListController.handle
 );
 
+userAclRoutes.post(
+  '/roles/:role_id/permissions',
+  createRolePermissionController.handle
+);
 export default userAclRoutes;
