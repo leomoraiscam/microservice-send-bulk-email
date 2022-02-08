63cb371fe0af82fb079967107bf5f706c05d9cb6
lmorais <leozonnn@gmail.com>
2022-02-08 10:33:15 -0300

    feat: implementation a new list method and modify logic to find contacts by tags

--- src/modules/contacts/repositories/IContactsRepository.ts
+++ src/modules/contacts/repositories/IContactsRepository.ts
@@ -1,10 +1,21 @@
 import Contact from '../infra/typeorm/entities/Contact';
 
+interface ICrateContacts {
+  email: string;
+  subscribed: boolean;
+}
+
+interface IOptions {
+  take?: number;
+  skip?: number;
+}
+
 interface IContactsRepository {
   findById(id: string): Promise<Contact>;
   findByEmail(email: string): Promise<Contact>;
   findByTags(tags: string[]): Promise<Contact[]>;
-  create(email: string): Promise<Contact>;
+  list({ skip, take }: IOptions): Promise<Contact[]>;
+  create(data: ICrateContacts): Promise<Contact>;
   save(contact: Contact): Promise<void>;
 }
 
--- src/modules/contacts/repositories/in-memory/ContactsRepositoryInMemory.ts
+++ src/modules/contacts/repositories/in-memory/ContactsRepositoryInMemory.ts
@@ -2,7 +2,18 @@ import { v4 as uuidv4 } from 'uuid';
 
 import Contact from '@modules/contacts/infra/typeorm/entities/Contact';
 
+import paginateArray from '../../utils/paginateArrayInMemory';
 import IContactsRepository from '../IContactsRepository';
+
+interface ICrateContacts {
+  email: string;
+  subscribed: boolean;
+}
+
+interface IOptions {
+  take?: number;
+  skip?: number;
+}
 
 class ContactsRepositoryInMemory implements IContactsRepository {
   private contacts: Contact[] = [];
@@ -18,25 +29,37 @@ class ContactsRepositoryInMemory implements IContactsRepository {
   async findByTags(tags: string[]): Promise<Contact[]> {
     let isTagIncludeInContact = false;
 
-    const contacts = this.contacts.filter((contact) => {
-      const tagsContacts = contact.tags.map((tag) => tag.id);
+    const contacts = this.contacts
+      .filter((contact) => {
+        const tagsContacts = contact.tags.map((tag) => tag.id);
 
-      for (const tagId of tagsContacts) {
-        isTagIncludeInContact = tags.includes(tagId);
-      }
+        for (const tagId of tagsContacts) {
+          isTagIncludeInContact = tags.includes(tagId);
+        }
 
-      return isTagIncludeInContact;
-    });
+        return isTagIncludeInContact;
+      })
+      .filter((contact) => contact.subscribed === true);
 
     return contacts;
   }
 
-  async create(email: string): Promise<Contact> {
+  async list({ skip, take }: IOptions): Promise<Contact[]> {
+    const takeValue = take || 1;
+    const skipValue = skip || 10;
+
+    const paginateContacts = paginateArray(this.contacts, skipValue, takeValue);
+
+    return paginateContacts;
+  }
+
+  async create({ email, subscribed }: ICrateContacts): Promise<Contact> {
     const contact = new Contact();
 
     Object.assign(contact, {
       id: uuidv4(),
       email,
+      subscribed,
       created_at: new Date(),
       updated_at: new Date(),
     });
