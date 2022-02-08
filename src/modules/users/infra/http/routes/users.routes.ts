c7a26c268c934d1832f8401bbcb7e843db79b024
lmorais <leozonnn@gmail.com>
2022-02-08 10:38:34 -0300

    chore: definition the new routes to users

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import ListUsersController from '@modules/users/infra/http/controllers/ListUsersController';
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
+
+const listUsersController = new ListUsersController();
 
 const usersRouter = Router();
 
+usersRouter.get('/', listUsersController.handle);
 usersRouter.post('/', UsersController.create);
 
 export default usersRouter;
