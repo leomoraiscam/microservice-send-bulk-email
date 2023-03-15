dac2b16d6b58e8c256d4d094a9b8d2b943934094
lmorais <lmorais.contato@gmail.com>
2023-03-14 23:28:56 -0300

    chore: remove preset flow from babel config

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-    '@babel/preset-flow'
+    '@babel/preset-typescript'
   ],
   plugins: [
     ['module-resolver', {
